9d377fe4377408d9ebadeb6a32502aa5296bf2da
toto0725 <katsuhiro.3730725@gmail.com>
2023-11-05 22:32:08 +0900

    バリデーションの設定２

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true     
   validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "は半角英数字混合で入力してください" }
+  validates :last_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
+  
 end
