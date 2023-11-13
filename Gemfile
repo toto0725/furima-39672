957d93c1db31e15355eae614be8a8f9267854966
toto0725 <katsuhiro.3730725@gmail.com>
2023-11-13 20:36:07 +0900

    new,createアクションの設定

--- Gemfile
+++ Gemfile
@@ -84,5 +84,7 @@ end
 
 gem 'devise'
 
+gem 'pry-rails'
+
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,21 @@ class ItemsController < ApplicationController
 def index
 end
 
+def new
+  @item = Item.new
+end
 
+def create
+  Item.create(item_params)
+  redirect_to '/'
+end
 
 
 
 private
 
-def message_params
-  params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+def item_params
+  params.require(:item).permit(:name, :info, :price, :category_id, :fee_id, :prefecture_id, :delivery_id, :).merge(user_id: current_user.id)
 end
 
 
