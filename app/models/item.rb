47aaffde39b475b0ab9e532ecb58546e256e375c
toto0725 <katsuhiro.3730725@gmail.com>
2023-11-13 23:36:20 +0900

    Active Hash　の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,18 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  belongs_to :Category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery
+  belongs_to :fee
+  belongs_to :prefecture
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
