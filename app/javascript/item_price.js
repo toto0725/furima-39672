b51c058a59aaa943aba5e9a8e08bf862c90d2787
toto0725 <katsuhiro.3730725@gmail.com>
2023-12-16 17:58:33 +0900

    修正３

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,9 +4,11 @@ window.addEventListener('turbo:load', () => {
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.10);
+    const taxPrice = Math.floor(inputValue * 0.10);  // 計算結果を切り捨て
+    addTaxDom.innerHTML = taxPrice;
 
     const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = Math.floor(inputValue * 0.90);
+    const profitValue = Math.floor(inputValue - taxPrice);  // 引き算で計算
+    profitDom.innerHTML = profitValue;
     })
  });
